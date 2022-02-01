0c49537df9286fbad461dc1c433650454daf79ba
cassandra-mn <arthenciac@gmail.com>
2022-02-01 14:26:59 -0300

    versão com quantidade de cartas informada

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ let quantidadeCartas = parseInt(prompt("Com quantas cartas você quer jogar?"));
 while (quantidadeCartas%2 !== 0 || quantidadeCartas < 4 || quantidadeCartas > 14) {
     quantidadeCartas = prompt("Insira um número par entre 4 e 14");
 }
+
+for (let cont = 0; cont < quantidadeCartas; cont++) {
+    var div = document.createElement('div'); 
+    document.querySelector(".cartas").appendChild(div);
+    div.classList.add("carta");
+}
