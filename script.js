cdae4f32b0da7861e1c5548ff12173de6616fec8
cassandra-mn <arthenciac@gmail.com>
2022-02-01 13:06:26 -0300

    versão que pede um número e só aceita um número válido

---
+++ script.js
@@ -0,0 +1,5 @@
+let quantidadeCartas = parseInt(prompt("Com quantas cartas você quer jogar?"));
+
+while (quantidadeCartas%2 !== 0 || quantidadeCartas < 4 || quantidadeCartas > 14) {
+    quantidadeCartas = prompt("Insira um número par entre 4 e 14");
+}
