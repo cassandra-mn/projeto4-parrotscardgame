8bc92d05ec81e94fb563a8137813a2c0f99ff6a9
cassandra-mn <arthenciac@gmail.com>
2022-02-02 12:07:00 -0300

    versão com virada de carta implementada

--- script.js
+++ script.js
@@ -1,18 +1,39 @@
 let quantidadeCartas = null;
 let arrayCartas = [];
 
-while (quantidadeCartas%2 !== 0 || quantidadeCartas < 4 || quantidadeCartas > 14) {
-    quantidadeCartas = parseInt(prompt("Com quantas cartas você quer jogar?"));
+iniciarJogo();
+
+function iniciarJogo() {
+    perguntarQuantidadeCartas();
+    arrayCartas.sort(comparador); 
+    distribuirCartas();
+    selecionar();
 }
 
-for (let cont = 0; cont < quantidadeCartas; cont++) {
-    arrayCartas[cont] = document.createElement('div'); 
-    document.querySelector(".cartas").appendChild(arrayCartas[cont]);
-    arrayCartas[cont].classList.add("carta");
+function perguntarQuantidadeCartas() {
+    while (quantidadeCartas%2 !== 0 || quantidadeCartas < 4 || quantidadeCartas > 14) {
+        quantidadeCartas = parseInt(prompt("Com quantas cartas você quer jogar?"));
+    }
 }
 
-arrayCartas.sort(comparador); 
+function distribuirCartas() {
+    for (let cont = 0; cont < quantidadeCartas; cont++) {
+        arrayCartas[cont] = document.createElement('div'); 
+        document.querySelector(".cartas").appendChild(arrayCartas[cont]);
+        arrayCartas[cont].classList.add("carta");
+        arrayCartas[cont].classList.add("verso");
+    }
+}
 
 function comparador() { 
     return Math.random() - 0.5; 
+}
+
+function selecionar() {
+    let elemento = document.querySelectorAll(".carta");
+    for (let cont = 0; cont < quantidadeCartas; cont++) {
+        elemento[cont].addEventListener('click', function() {
+            arrayCartas[cont].classList.toggle("frente");
+        });
+    }
 }
